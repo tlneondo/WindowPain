30e3f982b03910072a412b1bb2e5f4b3acb2949e
tlneondo <tlneondo@gmail.com>
2024-01-08 21:35:23 -0500

    Help I forgot how to program

--- main.c
+++ main.c
@@ -1,17 +1,15 @@
-#include "../includes/vdf.h"
+#include "./includes/vdf.h"
+#include <stdlib.h>
 
 int main(){
 
-    
+    struct vdf_object libraryPaths;
 
+    //libraryPaths = malloc(sizeof(struct vdf_object *));
     
+    
+    libraryPaths = vdf_parse_file("/home/icyjiub/.steam/steam/steamapps/libraryfolders.vdf");
+
+   return 0; 
 }
 
-int getSteamLibraryPaths(){
-
-///home/icyjiub/.steam/steam/steamapps/libraryfolders.vdf
-
-
-
-
-}
